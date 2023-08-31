14b8287d71a76401abc30069e51cc03ae3dfdfd9
Cansel İslamoğlu <bicanis53@gmail.com>
2023-08-31 16:38:08 +0300

    Initial commit added

---
+++ main.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+import tkinter.messagebox
+from PIL import ImageTk, Image
+import base64
+
+
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = (ord(clear[i]) + ord(key_c)) % 256
+        enc.append(enc_c)
+    encoded_bytes = bytes(enc)
+    encoded_string = base64.urlsafe_b64encode(encoded_bytes).decode()
+    return encoded_string
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc)
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + enc[i] - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
